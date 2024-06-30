e923987207c70922ecc171f12697544bf148d9e6
chandima2000 <hychandima2000@gmail.com>
2024-06-30 23:42:59 +0530

    create Google Cloud CLI functions

--- src/configuration/gcloud_syncer.py
+++ src/configuration/gcloud_syncer.py
@@ -1,1 +1,21 @@
 ## Download & Upload data from or to the Google Cloud
+
+import os
+
+
+class GCloudSync:
+
+    ## Upload to the GCP
+    def sync_folder_to_gcloud(self, gcp_bucket_url, filepath, filename):
+
+        command = f"gsutil cp {filepath}/{filename} gs://{gcp_bucket_url}/"
+        # command = f"gcloud storage cp {filepath}/{filename} gs://{gcp_bucket_url}/"
+        os.system(command)
+
+
+    ## Download from the GCP
+    def sync_folder_from_gcloud(self, gcp_bucket_url, filename, destination):
+
+        command = f"gsutil cp gs://{gcp_bucket_url}/{filename} {destination}/{filename}"
+        # command = f"gcloud storage cp gs://{gcp_bucket_url}/{filename} {destination}/{filename}"
+        os.system(command)
